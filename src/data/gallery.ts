0201170be0ed41244e7dcb15d8f2ab0683b49537
Sondre Sørbye <sondre.sorbye@gmail.com>
2026-09-21 16:36:06 +0000

    Crop pipekasse gallery image

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -24,8 +24,6 @@ export const galleryImages: GalleryImage[] = [
     src: "/images/prosjekt-pipekasse-sort.jpg",
     alt: "Svart pipebeslag med falser og topphatt montert på eldre takstein",
     caption: "Sort pipekasse",
-    zoom: 2,
-    zoomOrigin: "53% 48%",
   },
   {
     src: "/images/prosjekt-sort-pipebeslag-tegl.jpg",
